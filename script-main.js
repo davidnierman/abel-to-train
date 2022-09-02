d064019eb64aa57b3933a8bac833a027f83320c9
dnierman0920 <davidnierman93@gmail.com>
2022-09-02 11:36:03 -0700

    removing function only using data

--- script-main.js
+++ script-main.js
@@ -2,7 +2,7 @@
 //                              FORM  FUNCTIONS                              
 // #####################################################################
 
-function getRecaptchaResponse(response){
+function getRecaptchaResponse(response = ""){
   console.log("response: ", typeof response)
   if (typeof response !== "undefined"){
     return true
